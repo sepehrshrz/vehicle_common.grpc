ce7aee65699d61f7590214f73f4bc972cf6f2abd
sepehrshrz <sepehrshrz@gmail.com>
2026-07-29 13:24:51 +0330

    add php doc and make new device metric proto

--- proto.sh
+++ proto.sh
@@ -10,5 +10,17 @@ for d in protobuf/* ; do
     "--go_out=genproto/$service" --go_opt=paths=source_relative \
     --go-grpc_opt=require_unimplemented_servers=false \
     "--go-grpc_out=genproto/$service" --go-grpc_opt=paths=source_relative
+        # PHP
+    mkdir -p php
+
+    # Generate PHP only for shared contracts
+    if [[ "$service" == shared_* ]]; then
+        echo "Generating PHP for $service"
+
+        protoc \
+            --proto_path=protobuf \
+            "$d" \
+            --php_out=php
+    fi
 done
 
