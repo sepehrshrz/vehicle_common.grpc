7846b3a59b616414e14ce30ff6d1a11a23634d85
s.shirazi <sepehrshrz@gmail.com>
2026-09-26 13:30:50 +0330

    debug device proto 2

--- genproto/device/device.pb.go
+++ genproto/device/device.pb.go
@@ -67,7 +67,7 @@ func (x *GetDeviceByIMEIReq) GetDeviceImei() string {
 
 type GetDeviceByIMEIRes struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
-	Id            string                 `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Id            int64                  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Imei          string                 `protobuf:"bytes,2,opt,name=imei,proto3" json:"imei,omitempty"`
 	Mac           string                 `protobuf:"bytes,3,opt,name=mac,proto3" json:"mac,omitempty"`
 	SimNum        string                 `protobuf:"bytes,4,opt,name=sim_num,json=simNum,proto3" json:"sim_num,omitempty"`
@@ -112,11 +112,11 @@ func (*GetDeviceByIMEIRes) Descriptor() ([]byte, []int) {
 	return file_device_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *GetDeviceByIMEIRes) GetId() string {
+func (x *GetDeviceByIMEIRes) GetId() int64 {
 	if x != nil {
 		return x.Id
 	}
-	return ""
+	return 0
 }
 
 func (x *GetDeviceByIMEIRes) GetImei() string {
@@ -235,7 +235,7 @@ func (x *GetCommandByIDReq) GetCommandId() string {
 
 type GetCommandByIDRes struct {
 	state             protoimpl.MessageState `protogen:"open.v1"`
-	Id                int64                  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Id                string                 `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
 	CommandType       int64                  `protobuf:"varint,2,opt,name=command_type,json=commandType,proto3" json:"command_type,omitempty"`
 	Status            int64                  `protobuf:"varint,3,opt,name=status,proto3" json:"status,omitempty"`
 	DeviceId          int64                  `protobuf:"varint,4,opt,name=device_id,json=deviceId,proto3" json:"device_id,omitempty"`
@@ -279,11 +279,11 @@ func (*GetCommandByIDRes) Descriptor() ([]byte, []int) {
 	return file_device_proto_rawDescGZIP(), []int{3}
 }
 
-func (x *GetCommandByIDRes) GetId() int64 {
+func (x *GetCommandByIDRes) GetId() string {
 	if x != nil {
 		return x.Id
 	}
-	return 0
+	return ""
 }
 
 func (x *GetCommandByIDRes) GetCommandType() int64 {
@@ -358,7 +358,7 @@ const file_device_proto_rawDesc = "" +
 	"\vdevice_imei\x18\x01 \x01(\tR\n" +
 	"deviceImei\"\xc2\x02\n" +
 	"\x12GetDeviceByIMEIRes\x12\x0e\n" +
-	"\x02id\x18\x01 \x01(\tR\x02id\x12\x12\n" +
+	"\x02id\x18\x01 \x01(\x03R\x02id\x12\x12\n" +
 	"\x04imei\x18\x02 \x01(\tR\x04imei\x12\x10\n" +
 	"\x03mac\x18\x03 \x01(\tR\x03mac\x12\x17\n" +
 	"\asim_num\x18\x04 \x01(\tR\x06simNum\x12\x18\n" +
@@ -377,7 +377,7 @@ const file_device_proto_rawDesc = "" +
 	"\n" +
 	"command_id\x18\x01 \x01(\tR\tcommandId\"\xbe\x02\n" +
 	"\x11GetCommandByIDRes\x12\x0e\n" +
-	"\x02id\x18\x01 \x01(\x03R\x02id\x12!\n" +
+	"\x02id\x18\x01 \x01(\tR\x02id\x12!\n" +
 	"\fcommand_type\x18\x02 \x01(\x03R\vcommandType\x12\x16\n" +
 	"\x06status\x18\x03 \x01(\x03R\x06status\x12\x1b\n" +
 	"\tdevice_id\x18\x04 \x01(\x03R\bdeviceId\x12\x1d\n" +
